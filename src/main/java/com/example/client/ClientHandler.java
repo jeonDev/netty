844a4cb2334f94d33b4add35e0cd9a2f4d5bb23a
jeonDev <jjhwhdgus0727@gmail.com>
2024-10-30 22:32:47 +0900

    feat : Client

--- src/main/java/com/example/client/ClientHandler.java
+++ src/main/java/com/example/client/ClientHandler.java
@@ -1,18 +1,39 @@
-package com.example.netty.application;
+package com.example.client;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import lombok.extern.slf4j.Slf4j;
 
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
-public class ServerSocketHandler extends ChannelInboundHandlerAdapter {
+public class ClientHandler extends ChannelInboundHandlerAdapter {
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) {
+        log.info("channelActive");
+
+        String sendMessage = "Hello Netty";
+
+        ByteBuf messageBuffer = Unpooled.buffer();
+        messageBuffer.writeBytes(sendMessage.getBytes(StandardCharsets.UTF_8));
+
+        StringBuilder builder = new StringBuilder();
+        builder.append("전송한 문자열[");
+        builder.append(sendMessage);
+        builder.append("]");
+
+        log.info(builder.toString());
+        ctx.writeAndFlush(messageBuffer);
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         log.info("channelRead");
+
         String readMessage = ((ByteBuf)msg).toString(Charset.defaultCharset());
 
         StringBuilder builder = new StringBuilder();
@@ -28,12 +49,13 @@ public class ServerSocketHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) {
         log.info("channelReadComplete");
-        ctx.flush();
+        ctx.close();
     }
 
     @Override
     public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
         log.error("exceptionCaught");
+
         cause.printStackTrace();
         ctx.close();
     }
---
+++ src/main/java/com/example/client/ClientSocket.java
@@ -0,0 +1,36 @@
+package com.example.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class ClientSocket {
+
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new ClientHandler());
+                        }
+                    });
+
+            ChannelFuture f = b.connect("localhost", 10035).sync();
+
+            f.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
