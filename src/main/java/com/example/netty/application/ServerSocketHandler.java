36ba5942a555a2aa7a262cef50638f5b1292c696
jeonDev <jjhwhdgus0727@gmail.com>
2024-11-05 20:59:40 +0900

    fix : 메모리 누수 방지 및 채널 종료 처리

--- src/main/java/com/example/netty/application/ServerSocketHandler.java
+++ src/main/java/com/example/netty/application/ServerSocketHandler.java
@@ -3,6 +3,7 @@ package com.example.netty.application;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelHandler.Sharable;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -19,14 +20,26 @@ public class ServerSocketHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) {
         ByteBuf buf = (ByteBuf) msg;
-        String readMessage = buf.toString(Charset.forName("UTF-8"));
+        try {
+            String readMessage = buf.toString(Charset.forName("UTF-8"));
 
-        log.info("response {} {} : {}", buf.capacity(), readMessage.length(), readMessage.toString());
+            log.info("response {} {} : {}", buf.capacity(), readMessage.length(), readMessage.toString());
 
-        ByteBuf byteBuf = Unpooled.wrappedBuffer(readMessage.getBytes());
+            ByteBuf byteBuf = Unpooled.wrappedBuffer(readMessage.getBytes());
 
-        ChannelFuture future = ctx.writeAndFlush(byteBuf);
-        buf.release();
+            ChannelFuture future = ctx.writeAndFlush(byteBuf);
+            future.addListener((ChannelFutureListener) f -> {
+                if (!f.isSuccess()) {
+                    log.error("Fail");
+                    ctx.close();
+                }
+            });
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            ctx.close();
+        } finally {
+            buf.release();
+        }
     }
 
     @Override
